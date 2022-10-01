164f97c59b44f05c9191314bf85c5c705b26af0f
junayd-bhoyroo <hello@junaydbhoyroo.com>
2022-10-01 22:49:57 +0100

    begin adding functionality and edit styling
    added ability to upload audio file, file name is displayed in ui, added partial functionality to audio controls, time also updates to represent current time and total duration

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 **[work in progress]**
 
-##### A Spotify inspired web audio player for WebKit based browsers.
+### A Spotify inspired web audio player 
+#### (for WebKit based browsers)
+
+***
+
+I created this mini-project to learn more about using vanillaJS scripting with HTML.
--- script.js
+++ script.js
@@ -1,45 +1,99 @@
-const baseURL = window.location.origin
+let playing = false;
+const darkGrey = '#878787';
 
-const body = document.querySelector('body')
-const playButton = document.getElementById('play-pause')
-const playButtonImg = document.querySelector('#play-pause img')
-const rangeArea = document.getElementById('range-container')
-const range = document.getElementById('range')
+const root = document.documentElement;
+const body = document.querySelector('body');
+const audio = document.querySelector('audio');
+const playButton = document.getElementById('play-pause');
+const playButtonImg = document.querySelector('#play-pause img');
+const currentTime = document.getElementById('current-time');
+const totalDuration = document.getElementById('total-duration');
+const rangeArea = document.getElementById('range-container');
+const range = document.getElementById('range');
+const fileUpload = document.getElementById('file-upload');
+const trackName = document.getElementById('track-name');
+const nowPlaying = document.getElementById('now-playing');
 
 const getSliderPosition = (rangeInput) => {
-    const position = ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100
+    const position =
+        ((rangeInput.value - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
 
-    return position
-}
+    return position;
+};
+
+const formatTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+
+    const padSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${minutes}:${padSeconds}`;
+};
 
 const updateRangeColour = (colour1, colour2, position) => {
-    return `linear-gradient(to right, ${colour1} 0%, ${colour1} ${position}%, ${colour2} ${position}%, ${colour2} 100%)`
-}
+    return `linear-gradient(to right, ${colour1} 0%, ${colour1} ${position}%, ${colour2} ${position}%, ${colour2} 100%)`;
+};
 
-const changePlayButton = () => {
-    playButtonImg.src === `${baseURL}/png/play-button.png`
-        ? (playButtonImg.src = 'png/pause-button.png')
-        : (playButtonImg.src = 'png/play-button.png')
-}
+const play = () => {
+    playing = true;
+    playButtonImg.src = 'png/pause-button.png';
+    audio.play();
+};
+
+const pause = () => {
+    playing = false;
+    playButtonImg.src = 'png/play-button.png';
+    audio.pause();
+};
 
 const rangeMouseOver = () => {
-    range.style.overflow = 'visible'
-    range.style.background = updateRangeColour('#1D8954', '#878787', getSliderPosition(range))
-}
+    root.style.setProperty('--visibility', 'visible');
+    range.style.background = updateRangeColour('#1D8954', darkGrey, getSliderPosition(range));
+};
 
 const rangeMouseExit = () => {
-    range.style.overflow = 'hidden'
-    range.style.background = updateRangeColour('white', '#878787', getSliderPosition(range))
-}
+    root.style.setProperty('--visibility', 'hidden');
+    range.style.background = updateRangeColour('white', darkGrey, getSliderPosition(range));
+};
 
-const rangeInputColour = ({ target }) => {
-    range.style.background = updateRangeColour('#1D8954', '#878787', getSliderPosition(target))
-}
+const seek = () => {
+    audio.currentTime = range.value;
+    currentTime.innerHTML = formatTime(range.value);
+    range.style.background = updateRangeColour('#1D8954', darkGrey, getSliderPosition(range));
+};
 
-playButton.addEventListener('click', changePlayButton)
-rangeArea.addEventListener('mouseenter', rangeMouseOver)
-rangeArea.addEventListener('mouseleave', rangeMouseExit)
-range.addEventListener('input', rangeInputColour)
+const fileUploaded = ({ target }) => {
+    const regex = /.mp3|.wav|.aac|.flac|.m4a|.mp4|.wma/;
 
-const position = getSliderPosition(range)
-range.style.background = updateRangeColour('white', '#878787', position)
+    const audioFile = target.files.item(0);
+    audio.src = URL.createObjectURL(audioFile); // temp URL
+    audio.load();
+
+    trackName.innerHTML = audioFile.name.replace(regex, '');
+    nowPlaying.innerHTML = 'Now playing';
+};
+
+const setTotalDuration = () => {
+    range.max = audio.duration;
+    totalDuration.innerHTML = formatTime(audio.duration);
+};
+
+const updateTime = () => {
+    range.value = audio.currentTime;
+    updateRangeColour('white', darkGrey, audio.currentTime);
+    currentTime.innerHTML = formatTime(audio.currentTime);
+
+    console.log(range.value);
+};
+
+playButton.addEventListener('click', () => {
+    playing ? pause() : play();
+});
+audio.addEventListener('timeupdate', updateTime);
+audio.addEventListener('loadedmetadata', setTotalDuration);
+rangeArea.addEventListener('mouseenter', rangeMouseOver);
+rangeArea.addEventListener('mouseleave', rangeMouseExit);
+range.addEventListener('input', seek);
+fileUpload.addEventListener('input', fileUploaded);
+
+range.style.background = updateRangeColour('white', '#878787', getSliderPosition(range));
